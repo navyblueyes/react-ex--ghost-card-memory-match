547f59c678dce7b056d00d7c69ce8a7070c3d717
navyblueyes <lock_ed_on@yahoo.com>
2020-10-09 23:11:00 -0700

    finished CSS classes; started on react; issue with index of .map

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+// image for the pokemon
+// https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png
+
+const ghost = [
+  { id: 4, name: 'boo' },
+  { id: 10, name: 'candy' },
+  { id: 77, name: 'pumpkin' },
+  { id: 108, name: 'skull' },
+  { id: 132, name: 'smile' },
+  { id: 133, name: 'witch' },
+];
+
+const secondGhost = [...ghost, ...ghost];
+
+function ghostCard({ ghost }) {
+  return (
+    <div className="card" key={index}>
+      <div className="inner">
+        <div className="front">
+          <img 
+            src={`../public/ghosts/${ghost.id}.png`} 
+            alt={ghost.name}
+            width="100"
+          />
+        </div>
+        <div className="back">?</div>
+      </div>
+    </div>
+  )
+}
+
+export default function App() {
+  return <div className="app">
+    <div className="cards">
+      {secondGhost.map((ghost, i) =>(
+        <ghostCard key={i} ghost={ghost} />
+      ))}
+    </div>
+  </div>;
+}
